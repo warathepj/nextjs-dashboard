8aa4bdac51772156843e5333eb2f87f8ff8490fb
warathepj <80021220+warathepj@users.noreply.github.com>
2024-11-05 10:10:11 +0700

    /dashboard, /ui

---
+++ app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <div className='fixed left-0 top-0 h-full w-[100px] bg-slate-400'>SideNav</div>
+  )
+}
+
+export default SideNav
