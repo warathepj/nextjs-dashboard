66f357091416a5c5dd6c3c7acb77bd15412a9f94
warathepj <80021220+warathepj@users.noreply.github.com>
2024-11-06 21:07:24 +0700

    mod nav-links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,5 @@
 'use client';
-import {
-    UserGroupIcon,
-    HomeIcon,
-    DocumentDuplicateIcon,
-  } from '@heroicons/react/24/outline';
+
   import Link from 'next/link';
   import { usePathname } from 'next/navigation';
   import clsx from 'clsx';
